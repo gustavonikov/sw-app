9a615653f773b5c515574ab6b7d4d37feb273217
gustavonikov <gustavonikov@gmail.com>
2022-02-20 03:26:30 -0300

    feat: characters context component

---
+++ src/contexts/CharsContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react'
+import api from '../services/api'
+import Character from '../types/Characters'
+
+type CharactersContextProviderProps = {
+  children: ReactNode
+}
+
+type CharactersContextType = {
+  characters: Character[]
+  searchedCharName: string
+  onSearchChange: (param: string) => void
+}
+
+export const CharactersContext = createContext({} as CharactersContextType)
+
+export default function CharactersContextProvider({ children }: CharactersContextProviderProps) {
+  const [characters, setCharacters] = useState<Character[]>([])
+  const [searchedCharName, setSearchedCharName] = useState<string>('')
+
+  const filteredCharacters = characters
+    .filter(char => char.name.toLowerCase().includes(searchedCharName.toLowerCase()))
+
+  useEffect(() => {
+    api.getCharacters()
+      .then((chars) => {
+        setCharacters(chars)
+      })
+      .catch(error => console.log(error))
+  
+  }, [])
+
+  function onSearchChange(charName: string) {
+    setSearchedCharName(charName)
+  }
+
+  return (
+    <CharactersContext.Provider value={{ characters: filteredCharacters, searchedCharName, onSearchChange }}>
+      {children}
+    </CharactersContext.Provider>
+  )
+}
