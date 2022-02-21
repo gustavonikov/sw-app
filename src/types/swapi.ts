34f2a14875359ca7d2edc71a68fbe7dde0370e4a
gustavonikov <gustavonikov@gmail.com>
2022-02-21 14:18:02 -0300

    refactor: renaming file and adding more types

---
+++ src/types/swapi.ts
@@ -0,0 +1,34 @@
+export type RawCharacter = {
+	name: string
+	height: string
+	mass: string
+	hair_color: string
+	eye_color: string
+	birth_year: string
+	gender: string
+	homeworld: string
+	films: string[]
+	species: string[]
+	vehicles: string[]
+	starships: string[]
+	created: string
+	edited: string
+	url: string
+}
+
+export type CharactersData = {
+	count: number
+	next: string | null
+	previous: string | null
+	results: RawCharacter[]
+}
+
+export type Character = {
+	name: string
+	height: string
+	mass: string
+	hairColor: string
+	eyeColor: string
+	birthYear: string
+	homeworld: string
+}
