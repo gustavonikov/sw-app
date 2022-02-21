2b63a4ec3d24d8069c89c7d2772d560b7655a7ae
gustavonikov <gustavonikov@gmail.com>
2022-02-21 14:19:10 -0300

    feat: characters context

--- src/contexts/CharsContext.tsx
+++ src/contexts/CharsContext.tsx
@@ -1,40 +1,27 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
-import api from '../services/api'
-import Character from '../types/Characters'
+import { createContext, ReactNode, useState } from 'react'
 
 type CharactersContextProviderProps = {
   children: ReactNode
 }
 
 type CharactersContextType = {
-  characters: Character[]
-  onSearchChange: (param: string) => void
+  searchedCharName: string
+  onSearchChange: (charName: string) => void
 }
 
 export const CharactersContext = createContext({} as CharactersContextType)
 
 export default function CharactersContextProvider({ children }: CharactersContextProviderProps) {
-  const [characters, setCharacters] = useState<Character[]>([])
   const [searchedCharName, setSearchedCharName] = useState<string>('')
-
-  const filteredCharacters = characters
-    .filter(char => char.name.toLowerCase().includes(searchedCharName.toLowerCase()))
-
-  useEffect(() => {
-    api.getCharacters()
-      .then((chars) => {
-        setCharacters(chars)
-      })
-      .catch(error => console.log(error))
-  
-  }, [])
 
   function onSearchChange(charName: string) {
     setSearchedCharName(charName)
   }
 
   return (
-    <CharactersContext.Provider value={{ characters: filteredCharacters, onSearchChange }}>
+    <CharactersContext.Provider 
+      value={{ searchedCharName, onSearchChange }}
+    >
       {children}
     </CharactersContext.Provider>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import CharactersContextProvider from './contexts/CharsContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CharactersContextProvider>
+      <App />
+    </CharactersContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
