a8ba1337fda5c272a012d14f702b8faeaf3a8021
gustavonikov <gustavonikov@gmail.com>
2022-03-19 04:42:23 -0300

    fix: matchMedia error on jest mount function

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,11 @@
 import { configure } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
+global.matchMedia = global.matchMedia || function () {
+  return {
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  }
+}
+
 configure({ adapter: new Adapter() })
