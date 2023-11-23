ff35c603d0f0775b2034c057438444aefa8f3af3
gustavonikov <gustavonikov@gmail.com>
2023-11-23 02:57:15 -0300

    test: tests to do it

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,3 +4,20 @@ import App from '../App'
 test('renders App component', () => {
   expect(shallow(<App />)).toBeTruthy()
 })
+
+// se quando carregar o component chama a função getCharactersData
+// se seta loading como true quando começa e como false qndo termina
+// se quando dá erro exibe o component Result
+// se quando dá certo preenche os estados direitinho
+// e a imagem no header tá sendo carregada
+// se quando buscado um nome, o useEffect trigga chamando o getCharactersData
+
+
+/* test('if the characters data have name, height and mass only', () => {
+  const charactersProperties = Object.keys(charactersSample[0])
+
+  expect(charactersProperties.length).toEqual(3)
+  expect(charactersProperties.includes('name')).toBeTruthy()
+  expect(charactersProperties.includes('height')).toBeTruthy()
+  expect(charactersProperties.includes('mass')).toBeTruthy()
+}) */
